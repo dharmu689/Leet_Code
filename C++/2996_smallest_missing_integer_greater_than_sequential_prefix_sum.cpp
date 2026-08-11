a462719674dfd2318b7401e0248545df0989255b
dharmu689 <dharmukumar1622003@gmail.com>
2026-08-11 16:38:47 +0530

    Create 2996_smallest_missing_integer_greater_than_sequential_prefix_sum.cpp

---
+++ C++/2996_smallest_missing_integer_greater_than_sequential_prefix_sum.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    int missingInteger(vector<int>& nums)
+    {
+        int n = nums.size();
+        int sum = nums[0];
+        unordered_set<int> st(begin(nums), end(nums));
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i] == nums[i-1]+1)
+            {
+                sum += nums[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        while(st.count(sum))
+        {
+            sum++;
+        }
+        return sum;
+
+    }
+};
+
+int main()
+{
+    Solution obj;
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>nums[i];
+    }
+    int result = obj.missingInteger(nums);
+    cout<<result<<endl;
+    return 0;
+}
